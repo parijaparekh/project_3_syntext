08e1321d8c7f76d797a6daf4aacfd787ec1f5329
Parija Parekh <parija.parekh@gmail.com>
2023-02-06 00:54:26 +0800

    changes to understad problems with CORS

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,11 +1,11 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: (origin, callback) => {
+    origin: (origin, callback) => {    
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
-            callback(new Error('Not allowed by CORS'))
+            callback(new Error(`Not allowed by CORS and the origin is: ${origin}`))
         }
     },
     credentials: true,
