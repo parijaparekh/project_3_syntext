d731acaf5a773fec7be4b0334933eb6ed76b6d3a
Parija Parekh <parija.parekh@gmail.com>
2023-02-06 01:48:13 +0800

    fixing cors issue

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,8 +1,9 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: (origin, callback) => {    
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    origin: (origin, callback) => {   
+        //allowed origin check was removed just to support deployment. 
+        if (!origin) {
             callback(null, true)
         } else {
             const origin_check = allowedOrigins.indexOf(origin)
